56f33b8ef136cf56f2542b77cc5c7dc9fe5919fd
luigirovani <luigi3520@gmail.com>
2024-12-03 13:12:52 -0300

    Árvore transformada para usar 100% PO tipos de entrada sempre dentro do tipo do template lida com entrada inválida

--- avl.cpp
+++ avl.cpp
@@ -1,76 +1,43 @@
 #include <iostream>
 #include <locale>
+#include <limits>
 using namespace std;
 
-// Luigi Augusto Rovani RA 2266474
-// Lucas Vinícius Zuque de Lima RA 2268710
+// Luigi Augusto Rovani 
+// Lucas Vinícius Zuque de Lima
 // Maria Julia Leandro Leal da Rocha 
 
 template <typename T>
-class AVLNode {
-public:
-    T key;
-    int height;
-    AVLNode* left;
-    AVLNode* right;
-
-    AVLNode(T k) : key(k), height(1), left(nullptr), right(nullptr) {}
-};
-
-template <typename T>
 class AVLTree {
-public:
-    AVLNode<T>* insert(AVLNode<T>* root, T key) {
-        if (root == nullptr)
-            return new AVLNode<T>(key);
-
-        if (key < root->key)
-            root->left = insert(root->left, key);
-        else if (key > root->key)
-            root->right = insert(root->right, key);
-        else
-            return root;
-
-        root->height = 1 + max(getHeight(root->left), getHeight(root->right));
-        return balanceTree(root, key);
-    }
-
-    void clearTree(AVLNode<T>*& root) {
-        if (root == nullptr)
-            return;
-
-        clearTree(root->left);
-        clearTree(root->right);
-
-        delete root;
-        root = nullptr;
-    }
-
-    void printTree(AVLNode<T>* root, int space = 0, int level_gap = 5) const {
-        if (root == nullptr)
-            return;
-
-        space += level_gap;
-        printTree(root->right, space);
-
-        cout << endl;
-        for (int i = level_gap; i < space; ++i)
-            cout << " ";
-        cout << root->key << "\n";
-
-        printTree(root->left, space);
-    }
-
-    int getBalance(AVLNode<T>* node) const {
-        return node == nullptr ? 0 : getHeight(node->left) - getHeight(node->right);
-    }
-
 private:
-    int getHeight(AVLNode<T>* node) const {
-        return node == nullptr ? 0 : node->height;
+    class AVLNode {
+    public:
+        T key;
+        int height;
+        AVLNode* left;
+        AVLNode* right;
+
+        AVLNode(T k) : key(k), height(1), left(nullptr), right(nullptr) {}
+    };
+
+    AVLNode* root;
+
+    AVLNode* insertNode(AVLNode* node, T key) {
+        if (node == nullptr)
+            return new AVLNode(key);
+
+        if (key < node->key)
+            node->left = insertNode(node->left, key);
+        else if (key > node->key)
+            node->right = insertNode(node->right, key);
+        else
+            return node;
+
+        node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+        return balanceTree(node, key);
     }
 
-    AVLNode<T>* balanceTree(AVLNode<T>* root, T key) {
+    AVLNode* balanceTree(AVLNode* root, T key) {
         int balance = getBalance(root);
 
         if (balance > 1 && key < root->left->key) {
@@ -98,9 +65,9 @@ private:
         return root;
     }
 
-    AVLNode<T>* leftRotate(AVLNode<T>* z) {
-        AVLNode<T>* y = z->right;
-        AVLNode<T>* T2 = y->left;
+    AVLNode* leftRotate(AVLNode* z) {
+        AVLNode* y = z->right;
+        AVLNode* T2 = y->left;
 
         y->left = z;
         z->right = T2;
@@ -111,9 +78,9 @@ private:
         return y;
     }
 
-    AVLNode<T>* rightRotate(AVLNode<T>* z) {
-        AVLNode<T>* y = z->left;
-        AVLNode<T>* T3 = y->right;
+    AVLNode* rightRotate(AVLNode* z) {
+        AVLNode* y = z->left;
+        AVLNode* T3 = y->right;
 
         y->right = z;
         z->left = T3;
@@ -123,36 +90,114 @@ private:
 
         return y;
     }
+
+    int getHeight(AVLNode* node) const {
+        return node == nullptr ? 0 : node->height;
+    }
+
+    int getBalance(AVLNode* node) const {
+        return node == nullptr ? 0 : getHeight(node->left) - getHeight(node->right);
+    }
+
+    void clearTree(AVLNode*& node) {
+        if (node == nullptr)
+            return;
+
+        clearTree(node->left);
+        clearTree(node->right);
+
+        delete node;
+        node = nullptr;
+    }
+
+    void printTree(AVLNode* node, int space = 0, int level_gap = 5) const {
+        if (node == nullptr)
+            return;
+
+        space += level_gap;
+        printTree(node->right, space);
+
+        cout << endl;
+        for (int i = level_gap; i < space; ++i)
+            cout << " ";
+        cout << node->key << "\n";
+
+        printTree(node->left, space);
+    }
+
+
+public:
+    AVLTree() : root(nullptr) {}
+    ~AVLTree() {
+        clearTree(root);
+    }
+
+    void insert(T key) {
+        root = insertNode(root, key);
+    }
+
+    void clear() {
+        clearTree(root);
+        cout << "Árvore limpa!\n";
+    }
+
+    void print() const {
+        printTree(root);
+    }
+
+    int getBalance() const {
+        return getBalance(root);
+    }
+
+    T getInput() {
+        cout << "Digite o valor que deseja inserir: ";
+        T input;
+        cin >> input;
+
+        if (cin.fail()) {
+            cout << "Valor inválido!\nEstancie uma árvore de outro tipo\n" << endl;
+            printTip();
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return getInput();
+        }
+
+        cout << endl;
+        return input;
+    }
+
+    void printTip() const {
+        cout << "Digite um valor do tipo <" << typeid(T).name() << "> para inserir na árvore.\n";
+        cout << "Digite 0 para limpar a árvore.\n";
+        cout << "Digite -1 para sair.\n";
+        cout << "Digite -2 para imprimir a árvore.\n";
+        cout << endl;
+    }
 };
 
 int main() {
     setlocale(LC_ALL, "");
-
     AVLTree<int> tree;
-    AVLNode<int>* root = nullptr;
 
-    cout << "Use:\n 0 para obter o balanço e limpar a árvore \n -1 para sair \n -2 para printar a árvore" << endl;
+    tree.printTip();
 
     while (true) {
+        auto input = tree.getInput();
 
-        cout << "\nDigite o valor que deseja inserir: ";
-        int valor;
-        cin >> valor;
-
-        if (valor == 0) {
-			cout << "Bal: " << tree.getBalance(root) << endl;
-            tree.clearTree(root);
-            cout << "Árvore limpa!\n";
-        } else if (valor == -2) {
-            tree.printTree(root);
-		} else if (cin.fail() or valor == -1) {
+        if (input == 0) {
+            tree.clear();
+        }
+        else if (input == -2) {
+            tree.print();
+        }
+        else if (input == -1) {
             break;
-        } else {
-            root = tree.insert(root, valor);
+        }
+        else {
+            tree.insert(input);
         }
     }
 
     cout << "Fim" << endl;
-
     return 0;
 }
